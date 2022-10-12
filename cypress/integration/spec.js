1c240d94b4f5c96b4add046cfe66ba09dc4fa330
Chiamaka <hello.chiamaka@gmail.com>
2022-10-12 06:47:21 +0100

    added cypress package

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,17 +2,3 @@ const cypress = require("cypress");
 const { beforeEach } = require("mocha");
 const { describe } = require("mocha");
 
-describe('hrm demo account', () =>{
-    beforeEach(() =>{
-        cy.visit('/')
-    })
-    context('login', () =>{
-        it('allows a user to login with a valid email', ()=>{
-
-        })
-        it('does not allow a user to login with a valid email', ()=>{
-        
-        })
-    })
-    
-})
