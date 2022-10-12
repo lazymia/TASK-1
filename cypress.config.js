26bf5e9af163160e79168691af45b97f86661a9f
Chiamaka <hello.chiamaka@gmail.com>
2022-10-12 22:39:03 +0100

    additions

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "baseUrl": "",
+    "baseUrl": "https://restful-booker.herokuapp.com/",
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+
     },
-    chromeWebSecurity: false
+    chromeWebSecurity: false,
+    failOnStatusCode: false
   },
 });
--- cypress/integration/spec.js
+++
@@ -1,4 +0,0 @@
-const cypress = require("cypress");
-const { beforeEach } = require("mocha");
-const { describe } = require("mocha");
-
