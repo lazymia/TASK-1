2eb40633f1c8dbe1ff92930c7975ea041827b186
Chiamaka <hello.chiamaka@gmail.com>
2022-10-12 21:19:55 +0100

    scenario 2 and added comments

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
       // implement node event listeners here
       
     },
-    chromeWebSecurity: false
+    chromeWebSecurity: false,
+   "watchForFileChanges" : false
+
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,26 +4,35 @@ describe('MY STORE', () =>{
   })
 context('Users are able to search for an item using the search bar', () =>{
   it('allows a user search for items', () =>{
-    cy.get('#search_query_top').type("dress")
-    cy.get('#searchbox > .btn').click()
-    cy.url().and('eq','http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=dress&submit_search=')
-    cy.get('.page-heading').should("exist")
-    cy.get('.heading-counter').contains("7 results have been found.")
+    cy.get('#search_query_top').type("dress") // targets the search input field and types on dress
+    cy.get('#searchbox > .btn').click() // finds the search button and clicks on it
+    cy.url().and('eq','http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=dress&submit_search=') //asserts that the url has changed
+    cy.get('.page-heading').should("exist") //confirms that the search has been filtered
+    cy.get('.heading-counter').contains("7 results have been found.") //shows result of filtered search
   })
 })
  context('Users are able to filter search results under Women category by Color and Category', () =>{
-  it.only('allows users filter through women category', () =>{
-    cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.com/index.php?id_category=3&controller=category"]').click()
-    cy.url().and('eq', "http://automationpractice.com/index.php?id_category=3&controller=category")
-    cy.get('#layered_category_4').check()
-    cy.get(':nth-child(4) > .layered_color > a').click()
-    
+  it('allows users filter through women category', () =>{
+    cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.com/index.php?id_category=3&controller=category"]').click() //clicks on the women button
+    cy.url().and('eq', "http://automationpractice.com/index.php?id_category=3&controller=category") // assert that we are indeed in the women category
+    cy.get(':nth-child(1) > .layered_subtitle_heading > .layered_subtitle').should("contain.text", "Categories")  // confirmation that the categories section is available  
+    cy.get('#layered_category_4').check() //checking off the blouses
+    cy.get(':nth-child(3) > .layered_subtitle_heading > .layered_subtitle').should("exist") //checking that color section exist
+    cy.get(':nth-child(1) > .layered_color > a').click() //clicking on choiced color
+    cy.get('.last-item-of-tablet-line').should('be.visible').and('contain.text', 'blouse') //verifying that selections have been correctly filtered
   })
  })
 context('Users are able to view the details of any clothing item from the POPULAR section and add them to the cart', ()=>{
   it('allows users to view details of the popular section and add to cart', ()=>{
-    cy.get('.active > .homefeatured').and("contain.text", "POPULAR")
-      //.find("")
+    cy.get('.active > .homefeatured').should("exist")
+    cy.get('#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
+    cy.get('#layer_cart > .clearfix').should('exist')
+      .within(()=>{
+          cy.get('.layer_cart_product > h2').should('exist')
+          cy.get('h2 > .ajax_cart_product_txt_s').should('exist')
+          cy.get('[title="View my shopping cart"]').trigger({force:true})
+    })
+    
   })
 })
 })
