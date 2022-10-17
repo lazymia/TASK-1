c9824323ecb16bf6e6e663f3213ff63fd4b1b4e1
Chiamaka <hello.chiamaka@gmail.com>
2022-10-17 04:58:16 +0100

    reverted changes to file

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "baseUrl": "https://restful-booker.herokuapp.com/",
-    
+    "baseUrl" : "http://automationpractice.com/index.php",
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,42 +1,43 @@
-describe('API testing with Cypress', () =>{
-    beforeEach(()=>{
-        cy.request('/')    
+describe('MY STORE', () =>{
+
+    beforeEach(() =>{  
+        cy.visit('/')   
     })
-    it('creates booking for a valid customer using POST', ()=>{
-        cy.request({
-            method: 'POST',
-            url: '/booking',
-            body:{
-                "firstname" : "Jim",
-                "lastname" : "Brown",
-                "totalprice" : 111,
-                "depositpaid" : true,
-                "bookingdates" : {
-                    "checkin" : "2018-01-01",
-                    "checkout" : "2019-01-01"
-                },
-                "additionalneeds" : "Breakfast"
-            }
-        }).then((res)=>{
-            //expect(res.body).to.have.property('json');
-            expect(res.status).to.eql(200)
+    
+    context('Users are able to search for an item using the search bar', () =>{
+        it('allows a user search for items', () =>{
+            cy.get('#search_query_top').type("dress") // targets the search input field and types on dress     
+            cy.get('#searchbox > .btn').click() // finds the search button and clicks on it   
+            cy.url().and('eq','http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=dress&submit_search=') //asserts that the url has change    
+            cy.get('.page-heading').should("exist") //confirms that the search has been filtered   
+            cy.get('.heading-counter').contains("7 results have been found.") //shows result of filtered search
+            cy.get('.last-item-of-tablet-line').contains('dress')  
+        })   
+    })
+    
+    context('Users are able to filter search results under Women category by Color and Category', () =>{    
+        it('allows users filter through women category', () =>{    
+            cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.com/index.php?id_category=3&controller=category"]').click() //clicks on the women button
+            cy.url().and('eq', "http://automationpractice.com/index.php?id_category=3&controller=category") // assert that we are indeed in the women category
+            cy.get(':nth-child(1) > .layered_subtitle_heading > .layered_subtitle').should("contain.text", "Categories") // confirmation that the categories section is available   
+            cy.get('#layered_category_4').check() //checking off the blouses  
+            cy.get(':nth-child(3) > .layered_subtitle_heading > .layered_subtitle').should("exist") //checking that color section exist   
+            cy.get(':nth-child(1) > .layered_color > a').click() //clicking on choiced color   
+            cy.get('.last-item-of-tablet-line').should('be.visible').and('contain.text', 'blouse') //verifying that selections have been correctly filtered   
         })
     })
-    it.only('does not create booking for an invalid customer using POST', ()=>{
-        cy.request({
-            method: "POST",
-            url:"/booking",
-            body:{
-                "totalprice" : 111,
-                "depositpaid" : false,
-                "bookingdates" : {
-                    "checkin" : "2018-01-01",
-                    "checkout" : "2019-01-01"
-                },
-                "additionalneeds" : "Breakfast"
-            }
-        }).then((res)=>{
-            expect(res.body.status).to.be(500)
-        })
+    
+    context('Users are able to view the details of any clothing item from the POPULAR section and add them to the cart', ()=>{   
+        it('allows users to view details of the popular section and add to cart', ()=>{   
+          cy.get('.active > .homefeatured').should("exist")    
+          cy.get('#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()   
+          cy.get('#layer_cart > .clearfix').should('exist')    
+            .within(()=>{    
+          cy.get('.layer_cart_product > h2').should('exist')    
+          cy.get('h2 > .ajax_cart_product_txt_s').should('exist')    
+            })
+    
+        })    
     })
-})
+    
+    });
